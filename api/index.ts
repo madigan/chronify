6c10be082d528507c79c4d4f47e79a8d75fc4716
John Lynn <lynnjonathanp@gmail.com>
2025-06-05 15:52:41 -0400

    fix: migrate to vite-plugin-vercel

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default function GET(
+export default async function GET(
   request: VercelRequest,
   response: VercelResponse,
 ) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), vercel()],
 });
